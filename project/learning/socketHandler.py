aee6881e0822c747cf2cd57dd54cbccf586bf748
Erwan <vaillant.erwan49@gmail.com>
2024-04-02 22:36:25 +0200

    Socket persistence

--- project/learning/socketHandler.py
+++ project/learning/socketHandler.py
@@ -9,21 +9,20 @@ server_socket.bind(server_address)
 server_socket.listen(1)
 
 print("Attente de la connexion du client...")
+while True:
+    connection, client_address = server_socket.accept()
 
-connection, client_address = server_socket.accept()
+    try:
+        print("Connexion établie avec", client_address)
+        
+        # ? Matrice "jouet" pour tester la communication
+        # TODO: ici, il faut lancer une itération d'entrainement, puis remplacer data par la matrice des poids du nouveau réseau à éval
+        data = np.random.rand(3, 3, 3)
+        print(data)
+        
+        connection.sendall(str(data.shape).encode())
+        
+        connection.sendall(data.tobytes())
 
-try:
-    print("Connexion établie avec", client_address)
-    
-    # ? Matrice "jouet" pour tester la communication
-    # TODO: ici, il faut lancer une itération d'entrainement, puis remplacer data par la matrice des poids du nouveau réseau à éval
-    data = np.random.rand(3, 3, 3)
-    print(data)
-    
-    connection.sendall(str(data.shape).encode())
-    
-    connection.sendall(data.tobytes())
-
-finally:
-    connection.close()
-    server_socket.close()
+    finally:
+        connection.close()
